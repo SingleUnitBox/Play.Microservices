01ffdb5c322360354110462f9b7d9f0b7aa26e3c
Cezary Zukowski <cezaryzukowski90@gmail.com>
2025-10-03 22:18:49 +0100

    add CommandHandlerRetryDecorator

---
+++ Play.Common/src/Play.Common/Messaging/Resiliency/CommandHandlerRetryDecorator.cs
@@ -0,0 +1,30 @@
+﻿using Play.Common.Abs.Commands;
+using Play.Common.Settings;
+using Polly;
+
+namespace Play.Common.Messaging.Resiliency;
+
+public class CommandHandlerRetryDecorator<TCommand> : ICommandHandler<TCommand>
+    where TCommand : class, ICommand
+{
+    public ICommandHandler<TCommand> _innerHandler { get; set; }
+    public RetryStrategySettings _retrySettings { get; set; }
+    
+    public CommandHandlerRetryDecorator()
+    {
+        _retrySettings = new()
+        {
+            Delay = TimeSpan.FromSeconds(1),
+            MaxRetryAttempts = 
+        };
+    }
+    
+    public async Task HandleAsync(TCommand command)
+    {
+        var retryPolicy = new ResiliencePipelineBuilder()
+            .AddRetry(_retrySettings)
+            .Build();
+        
+        await retryPolicy.ExecuteAsync(async _ => await _innerHandler.HandleAsync(command));
+    }
+}
---
+++ Play.Common/src/Play.Common/Settings/RetryStrategySettings.cs
@@ -0,0 +1,8 @@
+﻿using Polly.Retry;
+
+namespace Play.Common.Settings;
+
+public class RetryStrategySettings : RetryStrategyOptions
+{
+    
+}
